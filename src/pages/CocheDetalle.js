5e609d64f55705c02708edfd9f8b9a7a97cefa97
Frank Nolasco Velasquez <49081234+FrankNolasco@users.noreply.github.com>
2023-01-26 10:54:31 -0500

    fix

--- src/pages/CocheDetalle.js
+++ src/pages/CocheDetalle.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
-import { Div, Heading, Row, Typography } from "../components/core/overrides";
+import { Div, Heading, Row } from "../components/core/overrides";
 import CocheEnRotacion from "../components/molecules/CocheEnRotacion";
 import ImagesCoches from "../components/molecules/ImagesCoches";
 import VideoCoches from "../components/molecules/VideoCoches";
 import Selector from "../components/molecules/Selector";
-import Scene from "../components/scena";
-import Button from "../components/atoms/Button";
+// import Scene from "../components/scena";
+// import Button from "../components/atoms/Button";
 
 const CocheDetalle = ({ value, onChange }) => {
+  const getParams = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get("id");
+    return myParam;
+  };
   const [idSeleccionado, setIdSeleccionado] = useState(1);
   return (
     <Div width="100%" padding="1em" style={{ boxSizing: "border-box" }}>
       <Row gap="10px">
-        <button
-          onClick={onChange}
-          style={{
-            padding: "10px",
-            border: "none",
-            background: "transparent",
-            cursor: "pointer",
-          }}
-        >
-          <i className="fa fa-angle-left" style={{ fontSize: "20px" }} />
-        </button>
+        {!getParams() && (
+          <button
+            onClick={onChange}
+            style={{
+              padding: "10px",
+              border: "none",
+              background: "transparent",
+              cursor: "pointer",
+            }}
+          >
+            <i className="fa fa-angle-left" style={{ fontSize: "20px" }} />
+          </button>
+        )}
         <Heading variant="h1">{value?.name}</Heading>
       </Row>
       <hr />
@@ -30,7 +37,7 @@ const CocheDetalle = ({ value, onChange }) => {
         <Selector
           options={[
             { id: 1, icon: "fa fa-car", name: "Exterior" },
-            { id: 2, icon: "fa fa-car", name: "Interior" },
+            // { id: 2, icon: "fa fa-car", name: "Interior" },
             { id: 3, icon: "fa fa-images", name: "Imagenes" },
             { id: 4, icon: "fa fa-photo-video", name: "Videos" },
           ]}
@@ -39,7 +46,7 @@ const CocheDetalle = ({ value, onChange }) => {
         />
         <Div width="100%" style={{ height: "70vh" }}>
           {idSeleccionado === 1 && <CocheEnRotacion colors={value?.colors} />}
-          {idSeleccionado === 2 && <Scene img={value?.interiorImg} />}
+          {/* {idSeleccionado === 2 && <Scene img={value?.interiorImg} />} */}
           {idSeleccionado === 3 && <ImagesCoches images={value?.images} />}
           {idSeleccionado === 4 && <VideoCoches video={value?.video} />}
         </Div>
